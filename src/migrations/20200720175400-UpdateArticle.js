fcb723415f7867fbe96fe4f4a0de9cba542be1c9
MichaelChan <zhinanchen@hk01.com>
2020-07-20 17:58:47 +0800

    feat: add `originalContent`

---
+++ src/migrations/20200720175400-UpdateArticle.js
@@ -0,0 +1,16 @@
+'use strict'
+const { DataTypes } = require('sequelize')
+
+const TABLE_NAME = 'articles'
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.addColumn(TABLE_NAME, 'original_content', {
+      type: DataTypes.TEXT,
+      allowNull: flase,
+    })
+  },
+  down: async queryInterface => {
+    await queryInterface.removeColumn(TABLE_NAME, 'original_content')
+  },
+}
