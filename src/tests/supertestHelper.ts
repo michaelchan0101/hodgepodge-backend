1049aed2a61263cfaeb477674cf32c707d9ce46c
MichaelChan <zhinanchen@hk01.com>
2020-07-20 10:43:23 +0800

    feat: add inject header in `supertestHelper`

--- src/tests/supertestHelper.ts
+++ src/tests/supertestHelper.ts
@@ -1,3 +1,16 @@
 import request from 'supertest'
+import jwt from 'jsonwebtoken'
+import config from '@/config'
+export const injectHeader = (payload: any) => {
+  const { debugSecret: secret } = config.jwt
+  const jwtDebug = jwt.sign(payload, secret, {
+    algorithm: 'HS256',
+    expiresIn: '14d',
+  })
+  return {
+    schema: 'DEBUG',
+    token: `Bearer ${jwtDebug}`,
+  }
+}
 
 export default request
