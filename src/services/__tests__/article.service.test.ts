93a7c9e3b59109a51a65af1f4f7b363aad3ca987
MichaelChan <zhinanchen@hk01.com>
2020-07-20 17:48:22 +0800

    feat: remove import article logic

--- src/services/__tests__/article.service.test.ts
+++ src/services/__tests__/article.service.test.ts
@@ -1,7 +1,5 @@
 import articleService from 'services/article.service'
 import fixtures from 'tests/fixtures'
-import config from '@/config'
-import path from 'path'
 
 describe('articleService', () => {
   beforeAll(async () => {
@@ -31,19 +29,5 @@ describe('articleService', () => {
     const article = await articleService.getArticle(1)
     expect(article.id).toEqual(1)
     expect(article.category.id).toEqual(1)
-  })
-
-  test('should batch import articles successfully', async () => {
-    const exampleData = [
-      {
-        categoryId: 1,
-        title: 'V8 垃圾回收机制',
-        path: path.join(config.article.basePath, 'javascript', 'V8 垃圾回收机制.md'),
-      },
-    ]
-    const articles = await articleService.batchImportArticles(exampleData)
-    expect(articles).toHaveLength(exampleData.length)
-    expect(articles[0].title).toEqual(exampleData[0].title)
-    expect(articles[0].createdAt).toEqual(articles[0].updatedAt)
   })
 })
